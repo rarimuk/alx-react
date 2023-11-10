216578094c19cc16f3e23dfecc2d30c806f24af6
rarimuk <arimukruth@gmail.com>
2023-11-10 13:04:11 +0000

    Task 3

--- 0x00-Webpack/task_3/webpack.config.js
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -1,68 +1,55 @@
 const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-	plugins: [
-		new HTMLWebpackPlugin({
-			filename: './index.html',
-		}),
-		new CleanWebpackPlugin(),
-		],
-	devtool: 'inline-source-map',
 	mode: 'development',
 	entry: {
-		header: {
-			import: './modules/header/header.js',
-			dependOn: 'shared',
-		},
-		body: {
-			import: './modules/body/body.js',
-			dependOn: 'shared',
-		},
-		footer: {
-			import: './modules/footer/footer.js',
-			dependOn: 'shared',
-		},
-		shared: 'jquery',
+		all: [
+			'./modules/header/header.js',
+			'./modules/body/body.js',
+			'./modules/footer/footer.js',
+		]
 	},
 	output: {
-		path: path.resolve(__dirname, 'public'),
 		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'public')
 	},
-	optimization: {
-		splitChunks: {
-			chunks: 'all',
-		},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader']
+			},
+			{
+				test: /\.(png|jpg|gif)$/i,
+				use: [
+					'file-loader',
+					{
+						loader: 'image-webpack-loader',
+						options: {
+							bypassOnDebug: true,
+							disable: true
+						},
+					},
+				],
+			},
+		]
 	},
+	devtool: 'inline-source-map',
 	devServer: {
 		static: path.join(__dirname, './public'),
-		open: true,
-		port: 8564,
+		compress: true,
+		port: 8564
 	},
-	performance: {
-		maxAssetSize: 1000000,
-	},  
-  module: {
-	rules: [
-		{
-			test: /\.css$/i,
-			use: ["css-loader", "style-loader"],
-		},
-		{
-			test: /\.(?:ico|gif|png|jpe?g|svg)$/i,
-			type: 'asset/resource',
-			use: [
-				"file-loader",
-				{
-					loader: "image-webpack-loader",
-					options: {
-							bypassingOnDebug: true,
-							disable: true,
-					},
-				},
-			],
-		},
+	plugins: [
+		new CleanWebpackPlugin(), // clean the public folder before building
+		new HtmlWebpackPlugin() // Generates default index.html
 	],
-},
+	optimization: {
+		splitChunks: {
+			chunks: 'all' // split all chunks
+		}
+	}
+	
 };
